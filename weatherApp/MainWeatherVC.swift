543625b3a74ee59b5998d164b2dbe448e4c070d5
dariusz guzowski <dariuszguzowski90@gmail.com>
2018-06-06 12:24:44 +0200

    add easyPeasy to all UI elements

---
+++ weatherApp/MainWeatherVC.swift
@@ -0,0 +1,83 @@
+//
+//  ViewController.swift
+//  weatherApp
+//
+//  Created by dariusz guzowski on 06.06.2018.
+//  Copyright © 2018 dariusz guzowski. All rights reserved.
+//
+
+import UIKit
+import EasyPeasy
+
+class MainWeatherVC: UIViewController {
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var currentTempLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var currentWeatherImage: UIImageView!
+    @IBOutlet weak var typeWeather: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+    func setupLayout() {
+        dateLabel.easy.layout(
+            Top(28),
+            Left(8),
+            Width(180),
+            Height(40)
+        )
+        
+        currentTempLabel.easy.layout(
+            Top(33).to(dateLabel),
+            Left(8),
+            Width(180),
+            Height(70)
+        )
+        
+        cityLabel.easy.layout(
+            Top(66).to(currentTempLabel),
+            Left(8),
+            Width(125),
+            Height(40)
+        )
+        
+        currentWeatherImage.easy.layout(
+            Top(70),
+            Right(8),
+            Width(145),
+            Height(145)
+        )
+        
+        typeWeather.easy.layout(
+            Top(28).to(currentWeatherImage),
+            Right(37),
+            Width(88),
+            Height(40)
+        )
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ weatherApp/WeatherCell.swift
@@ -0,0 +1,83 @@
+//
+//  WeatherCell.swift
+//  weatherApp
+//
+//  Created by dariusz guzowski on 06.06.2018.
+//  Copyright © 2018 dariusz guzowski. All rights reserved.
+//
+
+import UIKit
+import EasyPeasy
+
+class WeatherCell: UITableViewCell {
+
+    @IBOutlet weak var weatherImage: UIImageView!
+    @IBOutlet weak var dayLabel: UILabel!
+    @IBOutlet weak var typeWeather: UILabel!
+    @IBOutlet weak var hightTemp: UILabel!
+    @IBOutlet weak var lowTemp: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        setupLayout()
+        
+    }
+    
+    func setupLayout() {
+        weatherImage.easy.layout(
+            Top(15),
+            Left(15),
+            Width(55),
+            Height(55)
+        )
+        
+        dayLabel.easy.layout(
+            Top(15),
+            Left(32),
+            Width(105),
+            Height(25)
+        )
+        
+        typeWeather.easy.layout(
+            Top(5).to(dayLabel),
+            Left(32),
+            Width(105),
+            Height(25)
+        )
+        
+        hightTemp.easy.layout(
+            Top(15),
+            Right(36),
+            Width(75),
+            Height(25)
+        )
+        
+        lowTemp.easy.layout(
+            Top(5).to(hightTemp),
+            Right(36),
+            Width(75),
+            Height(25)
+        )
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
